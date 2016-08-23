6d9a159846272393a954a9fceb777811457206e7
Matthew Schultz <mschultz4@gmail.com>
2016-08-23 01:35:50 +0000

    fixed repeating

--- answers/firstNonRepeatingCharacter.js
+++ answers/firstNonRepeatingCharacter.js
@@ -11,31 +11,36 @@
  * @param {string} str String to test
  * @returns {string} 
  */
-function firstNonRepeatingCharacter(str){
-    if(typeof str !== "string"){
+function firstNonRepeatingCharacter(str) {
+    if (typeof str !== "string") {
         throw TypeError("Expected string, but got: " + Object.prototype.toString.call(str));
     }
 
     var stringArray = str.toLowerCase().split("");
-    var repeatingCharacters = []; 
+    var repeatingCharacters = [];
 
-    stringArray.sort().reduce(function(prev, curr){
-        prev === curr ? repeatingCharacters.push(curr) : null;
-        return curr;
-    });
+    if (str) {
+        stringArray.sort().reduce(function(prev, curr) {
+            prev === curr ? repeatingCharacters.push(curr) : null;
+            return curr;
+        });
 
-    for(var i=0; i < stringArray.length; i++){
-        if(repeatingCharacters.indexOf(str.toLowerCase().split('')[i]) < 0){
-            return str[i];
+        for (var i = 0; i < stringArray.length; i++) {
+            if (repeatingCharacters.indexOf(str.toLowerCase().split('')[i]) < 0) {
+                return str[i];
+            }
         }
     }
-
+        return "";
 }
 
-
-//Tests
-console.log(firstNonRepeatingCharacter("stabs") === "t");
-console.log(firstNonRepeatingCharacter("sTabs") === "T");
-console.log(firstNonRepeatingCharacter("Howdy doth")  === "w");
-try{firstNonRepeatingCharacter(234)}
-catch(error){console.log(error.message);}
+    //Tests
+    console.log(firstNonRepeatingCharacter("stabs") === "t");
+    console.log(firstNonRepeatingCharacter("sTabs") === "T");
+    console.log(firstNonRepeatingCharacter("Howdy doth") === "w");
+    try {
+        firstNonRepeatingCharacter(234)
+    }
+    catch (error) {
+        console.log(error.message);
+    }
